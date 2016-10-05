47db39dcf591c353f34ea05e9dc22a172bf6e340
xlicwb <jefferic1234@gmail.com>
2016-10-05 21:55:59 +0800

    修改header

--- template/src/components/module-head.vue
+++ template/src/components/module-head.vue
@@ -1,35 +1,28 @@
 <template>
 
-<header id="header">
-
-{{title}}
-
-</header>
+<header id="header" v-text="title"></header>
 
 </template>
 
 <script>
+    import Lib from 'assets/Lib.js'
 
-import Lib from 'assets/Lib.js'
-
-module.exports = {
-  props:{
-  	"title":{
-		type:String,
-		default:"标题"
-	}
-  }
-}
+    module.exports = {
+        props: {
+            "title": {
+                type: String,
+                default: "标题"
+            }
+        }
+    }
 </script>
 
 
 <style scoped>
-
-#header{
-color:#fff;
-padding:15px 0;
-background:#04BE02;
-text-align:center;
-}
-
+    #header {
+        color: #fff;
+        padding: 15px 0;
+        background: #04BE02;
+        text-align: center;
+    }
 </style>
