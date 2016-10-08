a8feeea087c2a8e620a2886da405cc0389b49fe2
xlicwb <jefferic1234@gmail.com>
2016-10-08 10:53:30 +0800

    按需加载vux样式

--- template/src/assets/css.vue
+++ template/src/assets/css.vue
@@ -1,5 +1,5 @@
-// this component is used only for building css
 <style>
 @import 'base.less';
-@import '~vux/dist/vux.css';
+@import '~vux/src/styles/index.less';
+/*@import '~vux/dist/vux.css';*/
 </style>
